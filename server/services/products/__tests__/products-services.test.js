b5c3d1ba6928738538a0b4ef2c4d1b528d98fe06
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-22 16:57:19 +0000

    add tests for products service

---
+++ server/services/products/__tests__/products-services.test.js
@@ -0,0 +1,34 @@
+const { search, searchById, getDescriptionById } = require('../products-service');
+const { productId, productTitle, descriptionFields} = require('../mocks/product');
+  
+function assertAuthor(response) {
+  expect(response.author.name).toBe('Guilherme');
+  expect(response.author.lastname).toBe('Pendezza');
+};
+  
+describe('Products Service', () => {
+  describe('search', () => {
+    it('should search products by a given query', async () => {
+      const response = await search('geladeira');
+      assertAuthor(response);
+      expect(response.items.length).toBe(4);
+    });
+  });
+
+  describe('searchById', () => {
+    it('should retrieve a product by id', async () => {
+      const response = await searchById(productId);
+
+      assertAuthor(response);
+      expect(response.item.title).toEqual(productTitle);
+    });
+  });
+
+  describe('getDescriptionById', () => {
+    it('should return product description by id', async () => {
+      const response = await getDescriptionById(productId);
+
+      expect(response).toHaveProperty(...descriptionFields);
+    });
+  });
+});
---
+++ server/services/products/mocks/product.js
@@ -0,0 +1,7 @@
+module.exports = {
+  productId: 'MLA772565193',
+  productTitle: 'Por Que A Vov Guardou A Calcinha Na Geladeira? : Max Wallac',
+  descriptionFields: [
+    'date_created', 'last_updated', 'plain_text', 'snapshot', 'text'
+  ]
+};
