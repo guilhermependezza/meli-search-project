deea0bb29a6a70065e50d2a13c8079d8df34bc4d
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-25 02:20:39 +0000

    add lint rule and lint webpack.config file

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,9 @@
-const { port } = require('../../constants/client');
-
 const HtmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const autoprefixer = require('autoprefixer');
 const path = require('path');
+
+const { port } = require('../../constants/client');
 
 const sourcePath = path.resolve(process.cwd(), 'client/src');
 const buildPath = path.resolve(process.cwd(), 'client');
@@ -11,7 +11,7 @@ const buildPath = path.resolve(process.cwd(), 'client');
 const config = {
   context: sourcePath,
   entry: {
-    app: ['babel-polyfill', 'index.js'],
+    app: ['babel-polyfill', 'index.jsx'],
   },
   devtool: 'source-map',
   output: {
@@ -29,57 +29,61 @@ const config = {
   resolve: {
     extensions: ['.js', '.jsx', '.scss'],
     modules: [
-        'node_modules',
-        sourcePath
+      'node_modules',
+      sourcePath
     ]
   },
 
   module: {
     rules: [
-        {
-            test: /\.jsx?$/,
-            include: sourcePath,
-            use: [
-                'babel-loader'
-            ]
-        },
+      {
+        test: /\.jsx?$/,
+        include: sourcePath,
+        use: [
+          'babel-loader'
+        ]
+      },
 
-        // configure eslint
-        {
-            test: /\.jsx?$/,
-            enforce: 'pre',
-            include: sourcePath,
-            loader: 'eslint-loader',
-            options: { configFile: `${process.cwd()}/.eslintrc.json` }
+      // configure eslint
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        include: sourcePath,
+        loader: 'eslint-loader',
+        options: { 
+          configFile: `${process.cwd()}/.eslintrc.json`,
+          fix: true // autofix eslint errors
         },
+            
+      },
 
-        // configure image loader
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: ['file-loader']
-        },
+      // configure image loader
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
 
-        //configure sass loader
-        {
-            test: /\.scss$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: { hmr: true },
-                },
-                'css-loader',
-                {
-                    loader: 'postcss-loader',
-                    options: { plugins: [autoprefixer] }
-                },
-                'sass-loader',
-            ],
-        }
+      //configure sass loader
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { hmr: true },
+          },
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: { plugins: [autoprefixer] }
+          },
+          'sass-loader',
+        ],
+      }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({
-        filename: '[name].[hash].css'
+      filename: '[name].[hash].css'
     }),
     new HtmlPlugin({
       template: path.resolve(__dirname, `${sourcePath}/index.html`),
@@ -92,7 +96,6 @@ const config = {
       }
     })
   ]
-
 };
 
 module.exports = config;
