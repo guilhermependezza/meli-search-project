80bf7b50de59be537c27ab0bfeda22211089cc23
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-26 00:44:16 +0000

    add a base css file and import it in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'styles/base.scss';
 import App from './components/app';
 
 render(<App />, document.querySelector('[data-js="root"]'));
