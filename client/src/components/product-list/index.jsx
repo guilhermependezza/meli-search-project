0eb919d239d3b8f3ba5a7cbc9b01262dedd75b66
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-29 06:10:40 +0000

    improve product item style

--- client/src/components/product-list/index.jsx
+++ client/src/components/product-list/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProductItem from 'components/product-item';
+import './product-list.scss';
 
 import productItemsMock from '../../mocks/product-list.json';
 
 export default function ProductList() {
   return (
-    <>
+    <div className="product-list">
       {productItemsMock.items.map(i => <ProductItem productData={i} />)}
-    </>
+    </div>
   );
 }
