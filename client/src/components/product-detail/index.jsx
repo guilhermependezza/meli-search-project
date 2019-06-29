aa75fe5408d894bafabe85a13789e8501e78f4e0
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-29 21:52:24 +0000

    build the product detail page

--- client/src/components/product-detail/index.jsx
+++ client/src/components/product-detail/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './product-detail.scss';
+
+import productDetail from '../../mocks/product-detail.json';
+import productDetailDescription from '../../mocks/product-detail-description.json';
 
 export default function ProductDetail() {
+  const { item } = productDetail;
   return (
-    <p>Product Detail</p>
+    <div className="product-detail">
+      <div className="product-detail-banner">
+        <figure className="product-figure">
+          <img className="product-figure-img" src={item.pictures[0].url} alt="" />
+        </figure>
+        <div className="product-data">
+          <h1 className="product-title">{item.title}</h1>
+          <p className="product-quantity">Quantidade: {item.available_quantity}</p>
+          <p className="product-price">$ {item.price}</p>
+          <button type="button">Comprar</button>
+        </div>
+      </div>
+      <div className="product-detail-description">
+        <h2 className="description-header">Descrição do produto</h2>
+        <p className="description-text">{productDetailDescription.plain_text}</p>
+      </div>
+    </div>
   );
 }
