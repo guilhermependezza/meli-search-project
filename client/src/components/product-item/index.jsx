19e7c2b186523e1f061e829d83840f06f761e7d1
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-30 01:54:01 +0000

    add link to product item

--- client/src/components/product-item/index.jsx
+++ client/src/components/product-item/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './product-item.scss';
 
 function ProductItem({ productData }) {
   const isFreeShipping = productData.shipping.free_shipping;
   return (
     <div className="product-item" key={productData.id}>
-      <img className="product-image" src={productData.thumbnail} alt={productData.title} />
-      <div className="product-data">
-        <p className="product-price">
-          {productData.price}
-          { isFreeShipping
-            ? <img className="free-shipping" src="/images/ic_shipping.png" alt="Frete Grátis" />
-            : null
-            }
-        </p>
-        <p className="seller-location">{productData.address.state_name}</p>
-        <p className="product-title">{productData.title}</p>
-      </div>
+      <Link to={`/items/${productData.id}`} className="product-link">
+        <img className="product-image" src={productData.thumbnail} alt={productData.title} />
+        <div className="product-data">
+          <p className="product-price">
+            {productData.price}
+            { isFreeShipping
+              ? <img className="free-shipping" src="/images/ic_shipping.png" alt="Frete Grátis" />
+              : null
+              }
+          </p>
+          <p className="seller-location">{productData.address.state_name}</p>
+          <p className="product-title">{productData.title}</p>
+        </div>
+      </Link>
     </div>
   );
 }
