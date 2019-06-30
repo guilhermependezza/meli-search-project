3f2682885b1f68f48294e1d695fa7faf0d98d3cf
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-06-30 20:45:52 +0000

    remove promises to use async/await

--- server/services/products/products-service.js
+++ server/services/products/products-service.js
@@ -1,49 +1,50 @@
 const request = require('request');
-const servicesConfig = require('../../../environment/services.json')
+const servicesConfig = require('../../../environment/services.json');
 
-function search(query, quantity = 4) {
-  const  { services: { apiUrl, endpoints: { search } } } = servicesConfig;
+function fetchURL(fetchURL) {
+  return new Promise(resolve => request
+    .get(fetchURL, (error, response, body) => {
+      resolve(JSON.parse(body));
+    }));
+}
+
+function buildResponseData({ responseBody, singleProduct = false, quantity }) {
+  const responseData = {
+    author: {
+      name: 'Guilherme',
+      lastname: 'Pendezza'
+    }
+  };
+
+  if (singleProduct) {
+    responseData.item = responseBody;
+    return responseData;
+  }
+
+  responseData.items = responseBody.results.slice(0, quantity);
+  return responseData;
+}
+
+async function search(query, quantity = 4) {
+  const { services: { apiUrl, endpoints: { search } } } = servicesConfig;
   const searchURL = `${apiUrl}${search}?q=${query}`;
-  return fetchURL(searchURL).then((responseBody) => buildResponseData({responseBody, quantity}));
-};
+  const responseBody = await fetchURL(searchURL);
+  return buildResponseData({ responseBody, quantity });
+}
 
-function searchById(id) {
-  const  { services: { apiUrl , endpoints: { product } } } = servicesConfig;
+async function searchById(id) {
+  const { services: { apiUrl, endpoints: { product } } } = servicesConfig;
   const productURL = `${apiUrl}${product}/${id}`;
   const singleProduct = true;
-  return fetchURL(productURL).then((responseBody) => buildResponseData({responseBody, singleProduct}));
-};
+  const responseBody = await fetchURL(productURL);
+  return buildResponseData({ responseBody, singleProduct });
+}
 
 function getDescriptionById(id) {
-  const  { services: { apiUrl , endpoints: { product, description } } } = servicesConfig;
+  const { services: { apiUrl, endpoints: { product, description } } } = servicesConfig;
   const productURL = `${apiUrl}${product}/${id}/${description}`;
   return fetchURL(productURL);
-};
-
-const fetchURL = (fetchURL) => {
-  return new Promise((resolve, reject) => {
-    return request.get(fetchURL, (error, response, body) => {
-      resolve(JSON.parse(body));
-    });
-  });
-};
-
-function buildResponseData({responseBody, singleProduct = false, quantity}) {
-    const responseData = {
-        author: {
-            name: 'Guilherme',
-            lastname: 'Pendezza'
-        }
-    }
-
-    if (singleProduct) {
-        responseData.item = responseBody;
-        return responseData;
-    }
-  
-    responseData.items = responseBody.results.slice(0, quantity);
-    return responseData;
-};
+}
 
 module.exports = {
   search,
