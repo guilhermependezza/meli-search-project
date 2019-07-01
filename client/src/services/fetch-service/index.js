2652cccb1a0d6ef1f7643adf0ea487fc27dfd20e
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-07-01 07:57:14 +0000

    refactor services

--- client/src/services/fetch-service/index.js
+++ client/src/services/fetch-service/index.js
@@ -1,17 +1,19 @@
-export function getSearchResults(query, callback) {
-  const searchUrl = `http://localhost:8080/api/items/?q=${query}`;
-  return fetch(searchUrl)
+function fetchUrl(url) {
+  return fetch(url)
     .then(res => res.json());
+}
+
+export function getSearchResults(query) {
+  const searchUrl = `http://localhost:8080/api/items/?q=${query}`;
+  return fetchUrl(searchUrl);
 }
 
 export function getProductData(productId) {
   const productDataUrl = `http://localhost:8080/api/items/${productId}`;
-  return fetch(productDataUrl)
-    .then(res => res.json());
+  return fetchUrl(productDataUrl);
 }
 
 export function getProductDescription(productId) {
   const productDescriptionUrl = `http://localhost:8080/api/items/${productId}/description`;
-  return fetch(productDescriptionUrl)
-    .then(res => res.json());
+  return fetchUrl(productDescriptionUrl);
 }
