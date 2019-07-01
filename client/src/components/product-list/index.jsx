a1608d25df5bb74b7b43e7e07a3213076b36f348
Guilherme Pendezza <guilherme_pendezza@hotmail.com>
2019-07-01 18:31:25 +0000

    add the new breadcrumb in the results list

--- client/src/components/product-list/index.jsx
+++ client/src/components/product-list/index.jsx
@@ -2,22 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ProductItem from 'components/product-item';
+import Breadcrumb from 'components/breadcrumb';
 import './product-list.scss';
 
 function ProductList({ searchResults }) {
+  console.log(searchResults);
+  console.log(searchResults.categories);
   return (
-    <div className="product-list">
-      {
-        searchResults
-          .items.map(i => <ProductItem productData={i} key={i.id} />)
-      }
-    </div>
+    <>
+      <Breadcrumb categories={searchResults.categories} />
+      <div className="product-list">
+        {
+          searchResults
+            .items.map(i => <ProductItem productData={i} key={i.id} />)
+        }
+      </div>
+    </>
   );
 }
 
 ProductList.propTypes = {
   searchResults: PropTypes.shape({
-    items: PropTypes.array
+    items: PropTypes.array.isRequired,
+    categories: PropTypes.arrayOf(PropTypes.string).isRequired
   }).isRequired
 };
 
